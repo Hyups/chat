eee0a3bd616d7ca7fb49880d6ba1efc02a618869
Hyups <5656sanghup@naver.com>
2017-05-06 01:00:01 +0900

    add nickname setting

--- app.js
+++ app.js
@@ -1,14 +1,46 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var userList = [];
 
 app.get('/', function(req, res) {
     res.sendfile('index.html');
 });
-io.on('connection', function(socket) {
 
-    socket.on('chat message', function(msg ) {
-        io.emit('chat message', msg );
+
+io.on('connection', function(socket) {
+    var joinedUser = false;
+    var nickname;
+
+    
+    socket.on('join', function(data) {
+        if (joinedUser) {
+            return false;
+        }
+        nickname = data;
+        userList.push(nickname);
+        socket.broadcast.emit('join', {
+            nickname: nickname,
+            userList: userList
+        });
+        joinedUser = true;
+    });
+    socket.on('chat message', function(data) {
+        io.emit('chat message', {
+            nickname: nickname,
+            msg: data
+        });
+    });
+    socket.on('disconnect', function() {
+        if (!joinedUser) {
+            return false;
+        }
+        var i = userList.indexOf(nickname);
+        userList.splice(i, 1);
+        socket.broadcast.emit('left', {
+            nickname: nickname,
+            userList: userList
+        });
     });
 });
 
